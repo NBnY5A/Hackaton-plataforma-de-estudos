8e0f696bd839ea40fbba91a9a39771bf4270a592
NBnY5A <victor.lito@a.ucb.br>
2026-03-18 19:02:14 -0300

    feat: Cria o Header da aplicação e a HomePage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import Home from "./pages/Home";
+
+const App = () => {
+  return <Home />;
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
