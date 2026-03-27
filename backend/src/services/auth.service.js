1b8b92c30e98bf3626a44746776da6a85ad3e6b3
NBnY5A <victor.lito@a.ucb.br>
2026-03-27 19:43:12 -0300

    chore: Altera a smensagens de erro para serem menos expressivas

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -6,11 +6,11 @@ import { findByEmail, createUser } from "./user.service.js";
 export const login = async (email, password) => {
     const user = await findByEmail(email);
     if (!user) {
-        throw new Error("User not found");
+        throw new Error("Usuário ou senha inválida");
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-        throw new Error("Invalid password");
+        throw new Error("Usuário ou senha inválida");
     }
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
@@ -21,7 +21,7 @@ export const login = async (email, password) => {
 export const register = async (name, email, password) => {
     const user = await findByEmail(email);
     if (user) {
-        throw new Error("User already exists");
+        throw new Error("Usuário já cadastrado");
     }
     return await createUser({ name, email, password });
 };
