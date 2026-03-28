62a3bc75cd6722fec29446ab7c28039adc9c8a73
NBnY5A <victor.lito@a.ucb.br>
2026-03-28 18:46:04 -0300

    fix: Corrige um pequeno detalhe de estilo no card e remove hook não utilizado

--- frontend/src/pages/tasks/Tasks.jsx
+++ frontend/src/pages/tasks/Tasks.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import styles from "./Tasks.module.css";
