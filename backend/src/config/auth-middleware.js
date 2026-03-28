5dc32e84416cf4fc48da4550d524d560869998a7
NBnY5A <victor.lito@a.ucb.br>
2026-03-28 18:49:04 -0300

    fix: Corrige o retorno do middleware para de message para error

--- backend/src/config/auth-middleware.js
+++ backend/src/config/auth-middleware.js
@@ -5,19 +5,19 @@ export function authMiddleware(req, res, next) {
     const authHeader = req.headers['authorization'];
 
     if (!authHeader) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ error: "Unauthorized" });
     }
 
     const token = authHeader?.split(' ')[1];
 
     if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ error: "Unauthorized" });
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.id;
         next();
     } catch (error) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ error: "Unauthorized" });
     }
 }
